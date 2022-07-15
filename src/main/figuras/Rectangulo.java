81144ed644ed68029fe7d8c8320fc1dafa22f2c8
Leonel Moreno <leonel.moreno@tsoftglobal.com>
2022-07-15 10:52:17 -0300

    probando

--- src/main/figuras/Rectangulo.java
+++ src/main/figuras/Rectangulo.java
@@ -3,6 +3,7 @@ package main.figuras;
 public class Rectangulo {
     private int largo;
     private int ancho;
+    int largo2;
 
     public Rectangulo(int largo, int ancho) {
         this.largo = largo;
